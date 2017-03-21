eeb7c3ddc17968f8775db2ca14a3fd0c579e0751
Frank <fnu.frangky@gmail.com>
2017-03-20 23:21:36 -0400

    changed movespeed

--- Tests/dev/frank/PlatformerGame/entities/creatures/CreatureTest.java
+++ Tests/dev/frank/PlatformerGame/entities/creatures/CreatureTest.java
@@ -13,6 +13,7 @@ import junit.framework.TestCase;
  *
  * @author Frank
  */
+//creature health test
 public class CreatureTest extends TestCase {
     
     public CreatureTest(String testName) {
@@ -22,13 +23,11 @@ public class CreatureTest extends TestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-    }
-    
+    }   
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
     }
-
     /**
      * Test of getHealth method, of class Creature.
      */
